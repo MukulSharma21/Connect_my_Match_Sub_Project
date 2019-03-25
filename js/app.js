6eb28cb9ea160d5dea2d7aee6761a7610dc9c3d9
MukulSharma21 <mymukul112@gmail.com>
2019-03-25 06:02:06 -0700

    all Work is completed :)

--- js/app.js
+++ js/app.js
@@ -1,13 +1,15 @@
-const btn =document.getElementById('course-btn');
-const hidden_section =document.getElementById('hidden');
+const btn = document.getElementById('course-btn');
+const hidden_section = document.getElementById('hidden');
 let num = 0;
 
 // Show the hidden section
-btn.addEventListener('click' , function(){
+btn.addEventListener('click', function() {
 	if (num === 0) {
 		hidden_section.style.display = "block";
-		num=num+1;
+		num = num + 1;
 	} else {
 		hidden_section.style.display = "none";
 	}
 });
+
+var sidebar = new StickySidebar('#scroll', {topSpacing:20});
